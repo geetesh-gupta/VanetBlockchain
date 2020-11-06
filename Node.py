0442a9364843ba561a5fb89d36db1502f1d1fd17
Geetesh Gupta <gupta.15@iitj.ac.in>
2020-11-06 09:18:05 +0530

    Create StaticNode class

--- Node.py
+++ Node.py
@@ -15,3 +15,9 @@ class Node:
     def set_velocity(self, dx, dy):
         self.dx = dx
         self.dy = dy
+
+
+class StaticNode(Node):
+
+    def __init__(self, x, y):
+        super().__init__(x, y, 0, 0)
